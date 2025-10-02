fa8daf25499daa60f77afdbe17781dd23f97dd80
SaifUrRehman2k <surgamer2007@gmail.com>
2025-10-03 00:49:37 +0500

    Integrated with firebase and Added signup

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = (props) => {
   return (
     <>
-      <div className='flex flex-col flex-wrap items-start gap-1 w-full self-center'>
+      <div className='flex flex-col flex-wrap items-start w-full self-center'>
         <label htmlFor={props.inputName} className='text-[12px] w-full  text-gray-700 font-[300] '>{props.inputName}</label>
-        <input type={props.inputType} className='outline-[1px] w-full  bg-gray-200 outline-gray-800 rounded-[0.2em] px-2 py-[1px]' placeholder={props.placeHolder} id={props.inputName} onChange={props.updatedVal} />
+        <input type={props.inputType} className='w-full shadow-gray-400 shadow-sm  bg-gray-300 rounded-[0.2em] px-2 py-1 outline-gray-400' placeholder={props.placeHolder} id={props.inputName} onChange={props.updatedVal} required />
       </div>
     </>
   )
 }
 
 export default Input
+
+export const Radio = (props) => {
+  const handleClick = () => {
+    props.updatedVal({ target: { value: props.radioValue } })
+  }
+
+
+  return (
+    <>
+      <div 
+        className={`flex flex-row flex-wrap items-baseline gap-1 w-[30%] h-[4em]  rounded-[0.7em] box-border p-2 shadow-gray-400 justify-between hover:cursor-pointer  ${props.radioValue === props.selectedVal ? 'bg-gray-600 shadow-lg text-gray-200': 'bg-gray-300 shadow-md text-gray-950'} `}
+        onClick={handleClick}>
+        <input 
+          type='radio'
+          checked={props.selectedVal == props.radioValue}
+          value={props.radioValue}
+          name={props.radioGroup}
+          className='w-max  bg-gray-200 outline-gray-800 rounded-[0.2em] px-2 py-[1px]'
+          id={props.inputTitle}
+          onChange={props.updatedVal}
+          required />
+        <img src={props.icon} className='w-[18px]' />
+
+        <label
+          htmlFor={props.inputTitle} 
+          className='text-[16px]/5 w-full font-[400] ' >
+            {props.inputTitle}
+        </label>
+
+      </div>
+    </>
+  )
+}
---
+++ src/Components/Signup/index.jsx
@@ -0,0 +1,126 @@
+import React, { use, useState } from 'react'
+import Input, { Radio } from '../Input'
+import Button, { Submit } from '../Button'
+import { db } from '../../firebase'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
+import teacherSvg from '../../assets/icons/teacher.svg'
+import adminSvg from '../../assets/icons/admin.svg'
+import studentSvg from '../../assets/icons/student.svg'
+
+
+const SignUp = () => {
+    const [emailValue, setEmailValue] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [selectedRole, setSelectedRole] = useState('')
+
+
+
+
+    // const pass = 'admin'
+    const updateEmailValue = (e) => {
+        setEmailValue(e.target.value)
+    }
+    const updatePasswordValue = (e) => {
+        setPassword(e.target.value)
+    }
+    const updateNameValue = (e) => {
+        setName(e.target.value)
+    }
+    const updateRoleValue = (e) => {
+        setSelectedRole(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = {
+            name: name,
+            role: selectedRole,
+            password: password,
+            email: emailValue,
+        }
+
+
+        try {
+            const usersData = await getDocs(collection(db, 'users'))
+            let emailExists = false
+            
+            usersData.forEach((doc) => {
+                const existingUser = doc.data();
+                if (existingUser.email == user.email) {
+                    emailExists = true
+                }
+            })
+
+            if (emailExists) {
+                console.log('user exist with the Email');
+
+            } else {
+                const docRef = addDoc(collection(db, 'users'))
+                console.log('user added with id: ' + docRef.id);
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }
+
+
+
+
+
+    return (
+        <>
+            <form className='w-[15em] h-full flex flex-col flex-wrap items-start justify-center gap-10 ' onSubmit={handleSubmit}>
+
+                <p className='text-[16px] font-[600]'>Create Account.</p>
+
+                <div className='w-full flex flex-col flex-wrap items-start gap-1'>
+                    {/* <Input type='text' placeHolder='Name' inputID='NAME' inputName='Name' /> */}
+                    <p className='text-[12px] text-gray-600'> Choose role:</p>
+
+                    <div className='flex flex-row flex-nowrap w-full justify-between mb-6' >
+                        <Radio 
+                        inputTitle='Admin' 
+                        radioValue='admin' 
+                        radioGroup='radioGroup1' 
+                        updatedVal={updateRoleValue} 
+                        selectedVal={selectedRole } 
+                        icon={adminSvg}
+                        />
+                        
+                        <Radio 
+                        inputTitle='Teacher' 
+                        radioValue='teacher' 
+                        radioGroup='radioGroup1' 
+                        updatedVal={updateRoleValue} 
+                        selectedVal={selectedRole } 
+                        icon={teacherSvg}
+                        />
+                        
+                        <Radio 
+                        inputTitle='Student' 
+                        radioValue='student' 
+                        radioGroup='radioGroup1' 
+                        updatedVal={updateRoleValue} 
+                        selectedVal={selectedRole } 
+                        icon={studentSvg}
+                        />
+
+                    </div>
+
+                    <Input inputType='text' placeHolder='Name' inputName='Name' updatedVal={updateNameValue} />
+                    <Input inputType='email' placeHolder='jondoe23@gmailcom' inputName='E-mail' updatedVal={updateEmailValue} />
+                    <Input inputType='password' placeHolder='Password' inputName='Password' updatedVal={updatePasswordValue} />
+                </div>
+
+                <Submit title='Create Account' />
+            </form>
+        </>
+    )
+}
+
+export default SignUp
+
