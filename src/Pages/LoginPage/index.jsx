3ae8ac06dc6d6451f1ea0de1c8da26ecb78ff0b8
Saif ur Rehman <surgamer2007@gmail.com>
2025-10-15 11:39:35 +0000

    fixed background

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 const LoginPage = () => {
     return (
-        <div className='w-screen h-screen bg-[url(../../../src/assets/images/stats-bg.jpg)] bg-cover bg-no-repeat bg-left text-gray-800'>
+        <div className='w-screen h-screen bg-[url(/images/stats-bg.jpg)] bg-cover bg-no-repeat bg-left text-gray-800'>
             <div className='w-full h-full backdrop-blur-md flex flex-row items-center justify-center'>
 
                 <div className='w-[20em] md:w-[25em]  h-[28em] flex items-center justify-center bg-gray-200 shadow-lg shadow-gray-500 rounded-2xl'>
