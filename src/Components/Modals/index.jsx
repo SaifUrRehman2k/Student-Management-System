926eca2a1a4a8bf509b44d0e3bd11039e9d3667c
SaifUrRehman2k <surgamer2007@gmail.com>
2025-12-04 10:46:36 +0500

    cleaned up few files

---
+++ src/Components/Modals/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { ButtonGroup } from '../Button'
+import { useDispatch } from 'react-redux'
+import { hideModal } from '../../redux/modalSlice'
+
+
+
+const Modal = ({ children }) => {
+    return (
+        <div className='fixed inset-0 z-[999] flex items-center justify-center'>
+            <div className='absolute inset-0 bg-gray-700 opacity-40'></div>
+
+            <div className='relative z-10 w-[30em] h-max flex flex-col flex-nowrap gap-8 rounded-2xl bg-white p-6 text-gray-800'>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export const DeleteModal = ({ item }) => {
+    return (
+        <>
+            <div className='fixed inset-0 z-[999] flex items-center justify-center'>
+                <div className='absolute inset-0 bg-gray-700 opacity-40'></div>
+
+                <div className='relative z-10 w-[30em] h-max flex flex-col flex-nowrap gap-8 rounded-2xl bg-white p-6 text-gray-800'>
+                    <h1 className="text-[2em] font-[700]">Are you Sure?</h1>
+                    <p className="flex flex-col text-[1.6em]">
+                        You want to delete the {item}?
+                        <small className="text-[0.6em] text-gray-600">
+                            The {item} will be deleted forever from the database & Auth.
+                        </small>
+                    </p>
+
+                    <ButtonGroup btn1Class={'bg-red-500 text-gray-100'} btn2Class={'border-[2px] border-red-500 text-gray-800'} title1='Edit' title2='Close' btn2Fucntiion={() => dispatch(hideModal())} />
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export const ProfileModal = ({ user }) => {
+    const dispatch = useDispatch()
+
+    return (
+        <>
+            <h1 className="text-[2em] font-[700]">Your Profile</h1>
+
+            <div className="flex flex-row items-center w-full justify-between">
+                <div className="w-[30%] aspect-square bg-gradient-to-br from-blue-700 to-blue-300 rounded-full flex items-center justify-center text-white font-semibold">
+                    <h1 className="text-6xl">{user?.first_name?.charAt(0)}</h1>
+                </div>
+
+                <div className="flex flex-col flex-wrap items-start w-[60%]">
+                    <div className="w-full flex flex-row flex-nowrap justify-between items-center border-b-[1px] border-gray-300 py-2">
+                        <h1 className="text-[1.3em] font-[500]">Name</h1>
+                        <small className="text-[1.2em] font-[400]">{`${user?.first_name} ${user?.last_name}`}</small>
+                    </div>
+                    <div className="w-full flex flex-row flex-nowrap justify-between items-center border-b-[1px] border-gray-300 py-2">
+                        <h1 className="text-[1.3em] font-[500]">Email</h1>
+                        <small className="text-[1.2em] font-[400]">{user?.email}</small>
+                    </div>
+                    <div className="w-full flex flex-row flex-nowrap justify-between items-center border-b-[1px] border-gray-300 py-2">
+                        <h1 className="text-[1.3em] font-[500]">Password</h1>
+                        <small className="text-[1.2em] font-[400]">{user?.password}</small>
+                    </div>
+                </div>
+            </div>
+            <ButtonGroup btn1Class={'bg-blue-500 text-gray-100'} btn2Class={'border-[2px] border-blue-500 text-gray-800'} title1='Edit' title2='Close' btn2Fucntiion={() => dispatch(hideModal())} />
+        </>
+    )
+}
+
+
+export default Modal
--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAGjqdJZuumJ1QzxtoZEUUy5VkOpVx-dyo",
   authDomain: "student-management-52dcf.firebaseapp.com",
